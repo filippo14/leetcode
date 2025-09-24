a1a6614df04d79a13ac94f10c70eec43a40ebe4f
Filippo Moret <filippomoret@gmail.com>
2025-09-24 23:13:07 +0000

    [LeetCode Sync] Runtime - 21 ms (22.73%), Memory - 22.6 MB (24.71%)

--- python/0049-group-anagrams/solution.py
+++ python/0049-group-anagrams/solution.py
@@ -1,20 +1,10 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        
-        # defaultdict automatically creates an empty list if the key doesn't exist 
-        anagrams = defaultdict(list)
-
-        for str in strs:
-            # sorting the string alphabetically and the characters are joined back to form a new string
-            # that will be the reference for anagrams
-            ref_str = "".join(sorted(str))
-
-            # appending the string to the list in the dict corresponding to the reference of the anagrams.
-            anagrams[ref_str].append(str)
-        
-        return list(anagrams.values())
-
-# Time complexity: O(m * nlogn)
-# Space complexity: O(mn)
-
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            res[tuple(count)].append(s)
+        return list(res.values())
 
